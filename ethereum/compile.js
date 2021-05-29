525015e01040d6a387d0831ee93dd5f74f288907
Richard <richardmenning@googlemail.com>
2021-05-29 21:05:37 +0200

    finished compile script

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -10,3 +10,10 @@ const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+  fs.outputJSONSync(
+    path.resolve(buildPath, contract.replace(':', '')),
+    output[contract]
+  );
+}
