22778c34e0263dfc491170917678e22801492e16
Richard <richardmenning@googlemail.com>
2021-05-31 22:13:24 +0200

    add more tests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -34,4 +34,39 @@ describe('Campaigns', () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+
+  it('marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it('allows people to contribute money and marks them as approvers', async () => {
+    await campaign.methods.contribute.send({
+      value: '200',
+      from: accounts[1],
+    });
+    const isContributor = await campaign.methods.approvers(accounts[1].call());
+    assert(isContributor);
+  });
+
+  it('requires a minimum contribution', async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: '5',
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('should allow a manager to make a payment request', async () => {
+    await campaign.methods
+      .createRequest('buy batteries', '100', accounts[1])
+      .send({ from: accounts[0], gas: '1000000' });
+
+    const request = await campaign.methods.requests(0).call();
+    assert.equal('buy batteries', request.description);
+  });
 });
